57bb02c5dc297f753038f98457cd3333d86bfd5e
6matko <ace@bbit.tech>
2021-10-23 20:17:31 +0300

    Added tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  // mode: 'jit',
+  purge: ['./src/**/*.{html,ts}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/forms')
+,require('@tailwindcss/line-clamp')
+,require('@tailwindcss/typography')
+],
+};
